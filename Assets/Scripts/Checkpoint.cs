7118fc93d8c583f4314515b58b88e0b8ad969559
Maxwellt <maksimvinnik@gmail.com>
2020-02-10 12:02:32 +0200

    38, 39. Deactivating checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer sr;
+
+    public Sprite cpOff, cpOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CheckpointController.instance.DeactivateCheckpoints();
+
+            sr.sprite = cpOn;
+        }
+    }
+
+    public void ResetCheckpoint()
+    {
+        sr.sprite = cpOff;
+    }
+}
