3ba8d95226dc82e9b6146ccc3e811b0e616567a3
Maxwellt <maksimvinnik@gmail.com>
2020-09-26 23:25:12 +0300

    Finish the game!!!!!!!

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    public string mainMenu;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
