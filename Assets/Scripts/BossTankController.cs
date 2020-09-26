a540955e8cf2492a92c63ac4d5521b4f4ac1e44c
Maxwellt <maksimvinnik@gmail.com>
2020-09-26 14:48:35 +0300

    107. Laying down mines

--- Assets/Scripts/BossTankController.cs
+++ Assets/Scripts/BossTankController.cs
@@ -10,20 +10,28 @@ public class BossTankController : MonoBehaviour
     };
 
     public BossState currentState;
-
     public Transform theTank;
     public Animator anim;
 
-    [Header("Movement")] public float moveSpeed;
+    [Header("Movement")]
+    public float moveSpeed;
     public Transform leftPoint, rightPoint;
     private bool moveRight;
+    
+    [Header("Mines")]
+    public GameObject mine;
+    public Transform minePoint;
+    public float timeBetweenMines;
+    private float mineCounter;
 
-    [Header("Shooting")] public GameObject bullet;
+    [Header("Shooting")]
+    public GameObject bullet;
     public Transform firePoint;
     public float timeBetweenShots;
     private float shotCounter;
 
-    [Header("Hurt")] public float hurtTime;
+    [Header("Hurt")]
+    public float hurtTime;
     private float hurtCounter;
     public GameObject hitBox;
 
@@ -45,6 +53,7 @@ public class BossTankController : MonoBehaviour
                     GameObject newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
                     newBullet.transform.localScale = theTank.localScale;
                 }
+
                 break;
             case BossState.Hurt:
                 if (hurtCounter > 0)
@@ -54,8 +63,10 @@ public class BossTankController : MonoBehaviour
                     if (hurtCounter <= 0)
                     {
                         currentState = BossState.Moving;
+                        mineCounter = 0;
                     }
                 }
+
                 break;
             case BossState.Moving:
                 if (moveRight)
@@ -66,7 +77,7 @@ public class BossTankController : MonoBehaviour
                     {
                         theTank.localScale = Vector3.one;
                         moveRight = false;
-                        
+
                         EndMovement();
                     }
                 }
@@ -76,12 +87,21 @@ public class BossTankController : MonoBehaviour
 
                     if (theTank.position.x < leftPoint.position.x)
                     {
-                        theTank.localScale = new Vector3(-1f,1f,1f);
+                        theTank.localScale = new Vector3(-1f, 1f, 1f);
                         moveRight = true;
-                       
+
                         EndMovement();
                     }
                 }
+
+                mineCounter -= Time.deltaTime;
+
+                if (mineCounter <= 0)
+                {
+                    mineCounter = timeBetweenMines;
+                    Instantiate(mine, minePoint.position, minePoint.rotation);
+                }
+
                 break;
             default:
                 break;
@@ -99,7 +119,7 @@ public class BossTankController : MonoBehaviour
     {
         currentState = BossState.Hurt;
         hurtCounter = hurtTime;
-        
+
         anim.SetTrigger("Hit");
     }
 
@@ -107,9 +127,9 @@ public class BossTankController : MonoBehaviour
     {
         currentState = BossState.Shooting;
         shotCounter = timeBetweenShots;
-        
+
         anim.SetTrigger("StopMoving");
-        
+
         hitBox.SetActive(true);
     }
 }
---
+++ Assets/Scripts/BossTankMine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTankMine : MonoBehaviour
+{
+    public GameObject explosion;
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            Instantiate(explosion, transform.position, transform.rotation);
+            PlayerHealthController.instance.DealDamage();
+        }
+    }
+}
